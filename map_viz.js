0ff7c0da78b4597c0c48ed2d396b62f70bc5c145
CharlesGuo <gchaozheng@gmail.com>
2019-04-11 17:33:06 +0800

    Refine Butotn Style

--- map_viz.js
+++ map_viz.js
@@ -21,12 +21,12 @@ $.getJSON('data/Shenzhen.json', function(sz_json){
         if(curLayer == 0) return bus_data[curHour];
         if(curLayer == 1) return taxi_data[curHour];
         if(curLayer == 2) return subway_data[curHour];
-        if(curLayer == -1) return {};
+        if(curLayer == -1) return [];
     }
     
     var option = {
     
-        mapbox:{
+        mapbox3D:{
             style: 'mapbox://styles/goochaozheng/cjtmxzx0x51aw1fpebmqa6dgm',
             center: [113.976607, 22.600341],
             zoom: 11,
@@ -34,7 +34,7 @@ $.getJSON('data/Shenzhen.json', function(sz_json){
         series:[
             {
             type: 'lines3D',
-            coordinateSystem: 'mapbox',
+            coordinateSystem: 'mapbox3D',
             effect: {
                 show: true,
                 trailWidth: 1.5,
@@ -80,7 +80,7 @@ $.getJSON('data/Shenzhen.json', function(sz_json){
     
     
     function redraw(){
-        
+
         var n_trailLength, n_lineOpacity, n_polyline;
         if(curLayer == 2){
             n_trailLength = 0.2;
@@ -226,7 +226,7 @@ $.getJSON('data/Shenzhen.json', function(sz_json){
             document.getElementById('taxiControlInput').checked = false;
             redraw();
         }else{
-            curlayer = -1;
+            curLayer = -1;
             redraw();
         }
     })
@@ -238,7 +238,7 @@ $.getJSON('data/Shenzhen.json', function(sz_json){
             document.getElementById('busControlInput').checked = false;
             redraw();
         }else{
-            curlayer = -1;
+            curLayer = -1;
             redraw();
         }
     })
@@ -250,181 +250,13 @@ $.getJSON('data/Shenzhen.json', function(sz_json){
             document.getElementById('subwayControlInput').checked = false;
             redraw();
         }else{
-            curlayer = -1;
+            curLayer = -1;
             redraw();
         }
     })
     
 
 })
-
-
-
-// //drag box & box count
-// var busVisibale = true;
-// var taxiVisibale = true;
-// var subwayVisibale = true;
-// var truckVisibale = true;
-
-// var canvas = map.getCanvasContainer();
-// var start;
-// var startLnglat;
-// var current;
-// var box = null;
-// var curbbox = null;
-
-// map.boxZoom.disable();
-
-// var boxPopup = new mapboxgl.Popup({
-//     closeButton: false
-// });
-
-// map.on('zoom', function(){
-//     if(box){
-//         boxPopup.remove();
-//         box.parentNode.removeChild(box);
-//         box = null;
-//         curbbox = null;
-//     }
-// })
-
-// map.on('mousedown', function(e){
-//     startLnglat = e.lngLat;
-// });
-
-// canvas.addEventListener('mousedown', mouseDown, true);
-
-// function mousePos(e) {
-//     var rect = canvas.getBoundingClientRect();
-//     return new mapboxgl.Point(
-//         e.clientX - rect.left - canvas.clientLeft,
-//         e.clientY - rect.top - canvas.clientTop
-//     );
-// }
-
-// function mouseDown(e) {
-
-//     if(box){
-//         boxPopup.remove();
-//         box.parentNode.removeChild(box);
-//         box = null;
-//         curbbox = null;
-//     }
-
-//     // Continue the rest of the function if the shiftkey is pressed.
-//     if (!(e.shiftKey && e.button === 0)) return;
-
-//     // Disable default drag zooming when the shift key is held down.
-//     map.dragPan.disable();
-     
-//     // Call functions for the following events
-//     document.addEventListener('mousemove', onMouseMove);
-//     document.addEventListener('mouseup', onMouseUp);
-//     document.addEventListener('keydown', onKeyDown);
-     
-//     // Capture the first xy coordinates
-//     start = mousePos(e);
-// }
-
-// function onMouseMove(e) {
-//     // Capture the ongoing xy coordinates
-//     current = mousePos(e);
-     
-//     // Append the box element if it doesnt exist
-//     if (!box) {
-//         box = document.createElement('div');
-//         box.classList.add('boxdraw');
-//         canvas.appendChild(box);
-//     }
-     
-//     var minX = Math.min(start.x, current.x),
-//     maxX = Math.max(start.x, current.x),
-//     minY = Math.min(start.y, current.y),
-//     maxY = Math.max(start.y, current.y);
-     
-//     // Adjust width and xy position of the box element ongoing
-//     var pos = 'translate(' + minX + 'px,' + minY + 'px)';
-//     box.style.transform = pos;
-//     box.style.WebkitTransform = pos;
-//     box.style.width = maxX - minX + 'px';
-//     box.style.height = maxY - minY + 'px';
-// }
-
-// function onMouseUp(e) {
-//     // Capture xy coordinates
-//     curbbox = [start, mousePos(e)]
-//     boxCount();
-// }
-
-// function onKeyDown(e) {
-//     // If the ESC key is pressed
-//     if (e.keyCode === 27) boxCount();
-// }
-
-// function boxCount() {
-//     // Remove these events now that finish has been called.
-//     document.removeEventListener('mousemove', onMouseMove);
-//     document.removeEventListener('keydown', onKeyDown);
-//     document.removeEventListener('mouseup', onMouseUp);
-     
-//     // If bbox exists. use this value as the argument for `queryRenderedFeatures`
-//     if (curbbox) {
-//         var busCount = map.queryRenderedFeatures(curbbox, { layers: ['bus_circle'] }).length;
-//         var taxiCount = map.queryRenderedFeatures(curbbox, { layers: ['taxi_circle'] }).length;
-//         var truckCount = map.queryRenderedFeatures(curbbox, { layers: ['truck_circle'] }).length;
-//         var subwayCount = 0;
-
-//         var subwayFeatures = map.queryRenderedFeatures(curbbox, { layers: ['subway_circle'] });
-//         for(var i=0; i<subwayFeatures.length; i++){
-//             subwayCount += subwayFeatures[i].properties.count;
-//         }
-
-//         var busRatio, taxiRatio, subwayRatio, truckRatio;
-//         var sum = busCount + taxiCount + truckCount + subwayCount;
-//         if(sum != 0){
-//             busRatio = (busCount * 100 / sum).toFixed(2);
-//             taxiRatio = (taxiCount * 100/ sum).toFixed(2);
-//             subwayRatio = (subwayCount * 100/ sum).toFixed(2);
-//             truckRatio = (truckCount * 100/ sum).toFixed(2);
-//         }else{
-//             busRatio = 0;
-//             taxiRatio = 0;
-//             subwayRatio = 0;
-//             truckRatio = 0;
-//         }
-
-
-//         var text = '<h6>Statistics</h6>';
-
-//         if(busVisibale){
-//             text += "<span class='bus'>Bus: </span><span>" + busRatio + "%</span><span> (" + busCount + ")</span><br/>";
-//         }
-//         if(taxiVisibale){
-//             text += "<span class='taxi'>Taxi: </span><span>" + taxiRatio + "%</span><span> (" + taxiCount + ")</span><br/>";
-//         }
-//         if(subwayVisibale){
-//             text += "<span class='subway'>Subway: </span><span>" + subwayRatio + "%</span><span> (" + subwayCount + ")</span><br/>";
-//         }
-//         if(truckVisibale){
-//             text +=  "<span class='truck'>Truck: </span><span>" + truckRatio + "%</span><span> (" + truckCount + ")</span><br/>";
-//         }
-    
-//         if(busVisibale || taxiVisibale || subwayVisibale || truckVisibale){
-//             boxPopup.setLngLat(startLnglat)
-//             .setHTML(text)
-//             .addTo(map);
-//         }
-         
-//         map.dragPan.enable();
-//     }
-
-// }
-
-// map.on('render', function(){
-//     if(curbbox){
-//         boxCount();
-//     }
-// })
 
 
 // //Subway popup
